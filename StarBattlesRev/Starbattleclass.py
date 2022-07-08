f2ba51b07ba3a72071164016388c750650d172ef
hifumisenpai3@gmail.com <hifumisenpai3@gmail.com>
2022-07-08 13:16:06 +0200

    tested constructor fixed islandConstructor now has diagonal islands as an option

--- StarBattlesRev/Starbattleclass.py
+++ StarBattlesRev/Starbattleclass.py
@@ -14,26 +14,28 @@ class StarBattle():
 
         self.islandConstructor()
 
-    def randConstructor(self):
+    def randConstructor(self):   ### prob isValid returns True on np.random.permutation approaches e^-2 as n tends to infinity
         self.strc=np.eye(self.size,dtype=int)
+        c=0
         while not self.isValid(self.strc):
-            print(self.strc)
+            c+=1
+            #print(self.strc)
             self.strc=np.eye(self.size,dtype=int)
             while not self.isValid(self.strc):
                 self.strc=np.random.permutation(self.strc)
             temp=np.eye(self.size,dtype=int)
-            for i in range(1,self.stars):
+            for i in range(1,self.stars):  ### empirical testing: n=10 ca 1/80000-1/300000 n=20 ca 1/20000-1/40000 for 2 stars (not efficient)
                 while not self.isValid(temp):
                         temp=np.random.permutation(temp)
                 self.strc=np.add(self.strc,temp)
-    
+        print(c)
     def detConstructor(self,cMet):
         self.strc=np.eye(self.size,dtype=int)
         self.strc=self.strc[cMet]
         if not self.isValid(self.strc):
             pass
 
-    def islandConstructor(self):
+    def islandConstructor(self, diag=False):
         self.islands=np.zeros((self.size,self.size),dtype=int)
         t=1
         for x in np.ndindex(np.shape(self.strc)):
@@ -43,9 +45,16 @@ class StarBattle():
         while np.isin(self.islands,0).any()==True:
             for x in np.ndindex(np.shape(self.strc)):    
                 if self.islands[x]==0:
-                    Y=self.islands[max(x[0]-1,0):min(x[0]+2,self.size),max(x[1]-1,0):min(x[1]+2,self.size)]
+                    if diag:
+                        Y=self.islands[max(x[0]-1,0):min(x[0]+2,self.size),max(x[1]-1,0):min(x[1]+2,self.size)]
+                    else:
+                        Y=[]
+                        if x[0]+1<=self.size-1: Y.append(self.islands[(x[0]+1,x[1])])
+                        if x[0]-1>=0:Y.append(self.islands[(x[0]-1,x[1])])
+                        if x[1]+1<=self.size-1:Y.append(self.islands[(x[0],x[1]+1)])
+                        if x[1]-1>=0:Y.append(self.islands[(x[0],x[1]-1)])
                     for t in range(1,self.size+1):
-                        R=random.choice((0,1,2))
+                        R=random.choices((0,1),weights=[self.size,1],k=1)[0]
                         if np.isin(Y,t).any() and R==1:
                             self.islands[x]=t
 
@@ -67,10 +76,11 @@ class StarBattle():
 def test():
     import matplotlib.pyplot as plt
 
-    x=StarBattle(20,starcount=2)
+    x=StarBattle(10)
     #print(x.islands)
     #input()
     print(x.strc)
 
     plt.imshow(x.islands)
     plt.show()
+
