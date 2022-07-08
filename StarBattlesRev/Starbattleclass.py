e5c94f3e64a3e9dc27a757bfaee76be4b8826569
hifumisenpai3@gmail.com <hifumisenpai3@gmail.com>
2022-07-08 23:53:57 +0200

    optimise isValid by 20% help

--- StarBattlesRev/Starbattleclass.py
+++ StarBattlesRev/Starbattleclass.py
@@ -7,19 +7,16 @@ class StarBattle():
         self.stars=starcount
         self.size=size
         self.cMet=cMet
-        if self.cMet=='random':
-            self.randConstructor()
-        else:
+        if type(self.cMet)==np.ndarray:
             self.detConstructor(self.cMet)
+        else:
+            self.randConstructor()
 
-        self.islandConstructor()
+        #self.islandConstructor()
 
-    def randConstructor(self):   ### prob isValid returns True on np.random.permutation approaches e^-2 as n tends to infinity
+    def randConstructor(self):  
         self.strc=np.eye(self.size,dtype=int)
-        c=0
-        while not self.isValid(self.strc):
-            c+=1
-            #print(self.strc)
+        while not self.isValid(self.strc): ### prob isValid returns True on np.random.permutation approaches e^-2 as n tends to infinity
             self.strc=np.eye(self.size,dtype=int)
             while not self.isValid(self.strc):
                 self.strc=np.random.permutation(self.strc)
@@ -28,7 +25,6 @@ class StarBattle():
                 while not self.isValid(temp):
                         temp=np.random.permutation(temp)
                 self.strc=np.add(self.strc,temp)
-        print(c)
     def detConstructor(self,cMet):
         self.strc=np.eye(self.size,dtype=int)
         self.strc=self.strc[cMet]
@@ -57,8 +53,25 @@ class StarBattle():
                         R=random.choices((0,1),weights=[self.size,1],k=1)[0]
                         if np.isin(Y,t).any() and R==1:
                             self.islands[x]=t
+        for st in range(1,self.stars):
+            print('hello')
 
     def isValid(self,X):
+        for index in np.ndindex(np.shape(X)):
+            if X[index]==1:
+                for epi in (-1,1):
+                    for epj in (-1,1):
+                            if self.size-1>=index[0]+epi>=0 and self.size-1>=epj+index[1]>=0 and not (epi==epj and epi==0) and not X[index[0]+epi,index[1]+epj]==0:
+                                self.Valid = False
+                                return self.Valid
+        if (X.sum(axis=0)==self.stars).all() and (X.sum(axis=1)==self.stars).all() and np.amax(X)==self.stars:
+            self.Valid = True
+        else:
+            self.Valid = False
+        return self.Valid
+
+
+    def isValid1(self,X):
         for index in np.ndindex(np.shape(X)):
             if X[index]==1:
                 for epi in (-1,0,1):
@@ -76,7 +89,7 @@ class StarBattle():
 def test():
     import matplotlib.pyplot as plt
 
-    x=StarBattle(10)
+    x=StarBattle(50,starcount=2)
     #print(x.islands)
     #input()
     print(x.strc)
@@ -84,3 +97,20 @@ def test():
     plt.imshow(x.islands)
     plt.show()
 
+def testisValid():
+    Y=[]
+    import time
+    import matplotlib.pyplot as plt
+
+    for i in range(1,100):
+        G=np.zeros((100))
+        for j in range(0,100):
+            X=np.random.permutation(i)
+            t=time.time()
+            StarBattle(i,cMet=X)
+            t=time.time()-t
+            G[j]=t
+        Y.append(G.sum()/100)
+    plt.plot(Y)
+    plt.show()
+    
